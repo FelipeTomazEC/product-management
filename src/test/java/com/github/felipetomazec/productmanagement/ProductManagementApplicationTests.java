403073972cbf80ab10d363598298c4b7ca364773
felipetomazec <felipetomaz2206@gmail.com>
2024-09-05 19:31:11 -0300

    chore: initial commit

---
+++ src/test/java/com/github/felipetomazec/productmanagement/ProductManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.felipetomazec.productmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
